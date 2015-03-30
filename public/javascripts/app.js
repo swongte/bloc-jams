537b7245ef1eef961cd9e30f84121dd6c1180052
Stephanie <swongte@hotmail.com>
2015-03-29 22:35:22 -0700

    Changed slider to update (music location) data on change.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -276,11 +276,11 @@ var changeAlbumView = function(album) {
    year: '1881',
    albumArtUrl: '/images/album-placeholder.png',
    songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
      ]
  };
 
@@ -433,6 +433,14 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
        this.setSong(this.currentAlbum, song);
 
      },
+      seek: function(time) {
+       // Checks to make sure that a sound file is playing before seeking.
+       if(currentSoundFile) {
+         // Uses a Buzz method to set the time of the song.
+         currentSoundFile.setTime(time);
+       }
+     },
+
      setSong: function(album, song) {
         if (currentSoundFile) {
           currentSoundFile.stop();
@@ -462,21 +470,47 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
         return offsetXPercent;
       }
 
+      var numberFromValue = function(value, defaultValue) {
+        if (typeof value === 'number') {
+          return value;
+        }
+ 
+        if(typeof value === 'undefined') {
+          return defaultValue;
+        }
+ 
+        if(typeof value === 'string') {
+          return Number(value);
+        }
+      }
+
     return {
      templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
      replace: true,
         restrict: 'E',
-          scope: {}, // Creates a scope that exists only in this directive.
+          scope: {
+            onChange: '&'
+            },
         link: function(scope, element, attributes) {
             // These values represent the progress into the song/volume bar, and its max value.
             // For now, we're supplying arbitrary initial and max values.
             scope.value = 0;
-            scope.max = 200;
+            scope.max = 100;
  
           var $seekBar = $(element);
+
+            attributes.$observe('value', function(newValue) {
+              scope.value = numberFromValue(newValue, 0);
+            });
+ 
+            attributes.$observe('max', function(newValue) {
+              scope.max = numberFromValue(newValue, 100) || 100;
+            });
  
             var percentString = function () {
-              var percent = Number(scope.value) / Number(scope.max) * 100;
+              var value = scope.value || 0;
+              var max = scope.max || 100;
+              percent = value / max * 100;
               return percent + "%";
             }
  
@@ -491,6 +525,7 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
             scope.onClickSlider = function(event) {
               var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
               scope.value = percent * scope.max;
+                notifyCallback(scope.value);
             }
 
             scope.trackThumb = function() {
@@ -498,6 +533,7 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
                 var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
                 scope.$apply(function(){
                   scope.value = percent * scope.max;
+                    notifyCallback(scope.value);
                 });
               });
  
@@ -506,7 +542,13 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
            $document.unbind('mousemove.thumb');
            $document.unbind('mouseup.thumb');
          });
-       };  
+       };
+
+            var notifyCallback = function(newValue) {
+              if(typeof scope.onChange === 'function') {
+                scope.onChange({value: newValue});
+              }
+            };  
           
  
           
