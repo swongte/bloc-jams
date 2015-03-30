8150459bf9e7fcf933cf76be633f03ee4fd233ad
Stephanie <swongte@hotmail.com>
2015-03-29 21:52:51 -0700

    Made the sliders animate 'The Angular Way'.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -76,8 +76,8 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
 
 blocJams.controller('Collection.controller', ['$scope','SongPlayer', function($scope, SongPlayer) {
    $scope.albums = [];
-   		for (var i = 0; i < 33; i++) {
-     	  $scope.albums.push(angular.copy(albumPicasso)); //produces 33 copies of the albums and adds to $scope.albums array.
+      for (var i = 0; i < 33; i++) {
+        $scope.albums.push(angular.copy(albumPicasso)); //produces 33 copies of the albums and adds to $scope.albums array.
    }
 
     $scope.playAlbum = function(album){
@@ -185,55 +185,66 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
    };
  });
 
-blocJams.directive('slider', function(){
+ blocJams.directive('slider', ['$document', function($document){
 
-      var updateSeekPercentage = function($seekBar, event) {
-      var barWidth = $seekBar.width();
-      var offsetX =  event.pageX - $seekBar.offset().left;
- 
-      var offsetXPercent = (offsetX  / $seekBar.width()) * 100;
-      offsetXPercent = Math.max(0, offsetXPercent);
-      offsetXPercent = Math.min(100, offsetXPercent);
- 
-      var percentageString = offsetXPercent + '%';
-      $seekBar.find('.fill').width(percentageString);
-      $seekBar.find('.thumb').css({left: percentageString});
-   }
- 
+      // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
+      var calculateSliderPercentFromMouseEvent = function($slider, event) {
+        var offsetX =  event.pageX - $slider.offset().left; // Distance from left
+        var sliderWidth = $slider.width(); // Width of slider
+        var offsetXPercent = (offsetX  / sliderWidth);
+        offsetXPercent = Math.max(0, offsetXPercent);
+        offsetXPercent = Math.min(1, offsetXPercent);
+        return offsetXPercent;
+      }
 
-
-   return {
+    return {
      templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
      replace: true,
         restrict: 'E',
+          scope: {}, // Creates a scope that exists only in this directive.
         link: function(scope, element, attributes) {
+            // These values represent the progress into the song/volume bar, and its max value.
+            // For now, we're supplying arbitrary initial and max values.
+            scope.value = 0;
+            scope.max = 200;
  
           var $seekBar = $(element);
  
-          $seekBar.click(function(event) {
-            updateSeekPercentage($seekBar, event);
-          });
+            var percentString = function () {
+              var percent = Number(scope.value) / Number(scope.max) * 100;
+              return percent + "%";
+            }
  
-          $seekBar.find('.thumb').mousedown(function(event){
-            $seekBar.addClass('no-animate');
+            scope.fillStyle = function() {
+              return {width: percentString()};
+            }
  
-            $(document).bind('mousemove.thumb', function(event){
-              updateSeekPercentage($seekBar, event);
-            });
+            scope.thumbStyle = function() {
+              return {left: percentString()};
+            }
+
+            scope.onClickSlider = function(event) {
+              var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+              scope.value = percent * scope.max;
+            }
+
+            scope.trackThumb = function() {
+              $document.bind('mousemove.thumb', function(event){
+                var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+                scope.$apply(function(){
+                  scope.value = percent * scope.max;
+                });
+              });
  
-            //cleanup
-            $(document).bind('mouseup.thumb', function(){
-              $seekBar.removeClass('no-animate');
-              $(document).unbind('mousemove.thumb');
-              $(document).unbind('mouseup.thumb');
-            });
+         //cleanup
+         $document.bind('mouseup.thumb', function(){
+           $document.unbind('mousemove.thumb');
+           $document.unbind('mouseup.thumb');
+         });
+       };  
+          
  
-          });
+          
         }
    };
- });
-
-
-
-
-
+ }]);
