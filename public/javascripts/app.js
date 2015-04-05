a8598617fa5b241c62f9c8f4980da5b67ab449b9
Stephanie <swongte@hotmail.com>
2015-04-05 15:23:57 -0700

    Added a time indicator for song progress

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -388,9 +388,16 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
 
 blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
+
+    SongPlayer.onTimeUpdate(function(event, time){
+     $scope.$apply(function(){
+       $scope.playTime = time;
+     });
+   });
+
 }]);
  
- blocJams.service('SongPlayer', function() {
+ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
       var currentSoundFile = null;
 
       var trackIndex = function(album, song) {
@@ -441,24 +448,65 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
        }
      },
 
+      onTimeUpdate: function(callback) {
+      return $rootScope.$on('sound:timeupdate', callback);
+    },
+
      setSong: function(album, song) {
         if (currentSoundFile) {
           currentSoundFile.stop();
         }
        this.currentAlbum = album;
        this.currentSong = song;
+
         currentSoundFile = new buzz.sound(song.audioUrl, {
           formats: [ "mp3" ],
           preload: true
         });
+
+        currentSoundFile.bind('timeupdate', function(e){
+        $rootScope.$broadcast('sound:timeupdate', this.getTime());
+      });
  
     this.play();
 
      }
    };
- });
+ }]);
 
  blocJams.directive('slider', ['$document', function($document){
+
+/*
+      blocJams.filter('timecode', function(){
+        return function(seconds) {
+          seconds = Number.parseFloat(seconds);
+ 
+          // Returned when no time is provided.
+          if (Number.isNaN(seconds)) {
+            return '-:--';
+          }
+ 
+          // make it a whole number
+          var wholeSeconds = Math.floor(seconds);
+ 
+          var minutes = Math.floor(wholeSeconds / 60);
+ 
+          remainingSeconds = wholeSeconds % 60;
+ 
+          var output = minutes + ':';
+ 
+          // zero pad seconds, so 9 seconds should be :09
+          if (remainingSeconds < 10) {
+            output += '0';
+          }
+ 
+          output += remainingSeconds;
+ 
+          return output;
+        }
+      })
+
+*/
 
       // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
       var calculateSliderPercentFromMouseEvent = function($slider, event) {
@@ -555,6 +603,36 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
         }
    };
  }]);
+
+blocJams.filter('timecode', function(){
+        return function(seconds) {
+          seconds = Number.parseFloat(seconds);
+ 
+          // Returned when no time is provided.
+          if (Number.isNaN(seconds)) {
+            return '-:--';
+          }
+ 
+          // make it a whole number
+          var wholeSeconds = Math.floor(seconds);
+ 
+          var minutes = Math.floor(wholeSeconds / 60);
+ 
+          remainingSeconds = wholeSeconds % 60;
+ 
+          var output = minutes + ':';
+ 
+          // zero pad seconds, so 9 seconds should be :09
+          if (remainingSeconds < 10) {
+            output += '0';
+          }
+ 
+          output += remainingSeconds;
+ 
+          return output;
+        }
+      })
+
 
 });
 
